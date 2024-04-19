b1982cfccf92ab649dd73a65e58e7060a29778b0
Specticall <josephyusmita@gmail.com>
2024-04-19 21:45:33 +0700

    feat: task view outlet done, about to make changes to the schema (adding a completedAt field for task

--- client/src/components/app/task/AcceptedTask.tsx
+++ client/src/components/app/task/AcceptedTask.tsx
@@ -1,33 +1,73 @@
 import { ScrollArea } from "@/components/ui/scrollable";
 import TaskCard from "./TaskCard";
-import { Tooltip } from "@/components/general/Tooltip";
-import { getMoodColor } from "@/lib/utils";
-import { TMood } from "@/context/MoodContext";
 import useTaskMutation from "@/hooks/useTaskMutation";
 import { Skeleton } from "@/components/general/Skeleton";
+import { TUserTask } from "@/types/general";
+type TGroupedTask = {
+  onGoing: TUserTask[];
+  completed: TUserTask[];
+};
+
+function groupTaskByStatus(task?: TUserTask[]) {
+  if (!task) return undefined;
+
+  return task.reduce(
+    (grouped: TGroupedTask, task) => {
+      if (task.status === "OnGoing") {
+        grouped.onGoing.push(task);
+        return grouped;
+      }
+
+      grouped.completed.push(task);
+      return grouped;
+    },
+    {
+      onGoing: [],
+      completed: [],
+    }
+  );
+}
 
 export default function AcceptedTask() {
-  const { onGoingTask } = useTaskMutation();
-
+  const { taskQuery } = useTaskMutation();
+  const groupedTask = groupTaskByStatus(taskQuery.data);
   return (
-    <ScrollArea className="bg-white-soft rounded-xl h-full">
-      <Skeleton isLoading={!onGoingTask}>
-        {onGoingTask && (
-          <div className=" flex flex-col gap-8 py-8">
-            {onGoingTask.map((task) => {
-              return (
-                <li className="px-8" key={task.id}>
-                  <TaskCard
-                    badgeColor={getMoodColor(task.mood as TMood)}
-                    task={task}
-                    className="shadow-xl shadow-accent/5"
-                  />
-                </li>
-              );
-            })}
+    <ScrollArea className="bg-white-soft rounded-xl h-0 min-h-full dotted-grid">
+      <Skeleton isLoading={!groupedTask}>
+        {groupedTask && (
+          <div className="grid grid-cols-2 gap-12 px-12">
+            <ul className=" flex flex-col gap-12 py-12">
+              <CategoryIndicator
+                count={groupedTask.onGoing.length}
+                text="On Going"
+              />
+              {groupedTask.onGoing.map((task) => {
+                return <TaskCard task={task} key={task.id} />;
+              })}
+            </ul>
+            <ul className=" flex flex-col gap-12 py-12">
+              <CategoryIndicator
+                count={groupedTask.completed.length}
+                text="Completed"
+              />
+              {groupedTask.completed.map((task) => {
+                return <TaskCard task={task} key={task.id} />;
+              })}
+            </ul>
           </div>
         )}
       </Skeleton>
     </ScrollArea>
+  );
+}
+
+function CategoryIndicator({ count, text }: { count: number; text: string }) {
+  return (
+    <p className="text-light flex items-center justify-start gap-3 text-[1rem] mb-[-1.5rem]">
+      {text}
+      <div className="text-dark bg-white-soft p-2 rounded-md border-[1px] border-border w-6 h-6 flex items-center justify-center">
+        {count}
+      </div>
+    </p>
   );
 }
--- client/src/components/app/task/TaskCard.tsx
+++ client/src/components/app/task/TaskCard.tsx
@@ -1,69 +1,103 @@
-import Button from "@/components/general/Button";
+import { TMood } from "@/context/MoodContext";
 import useTaskDetail from "@/hooks/useTaskDetail";
-import { cn } from "@/lib/utils";
-import { TTask } from "@/types/general";
+import { cn, getMoodColor } from "@/lib/utils";
+import { TUserTask } from "@/types/general";
+// export default function TaskCard({
+//   task,
+//   className,
+//   badgeColor,
+//   enableCompleteButton,
+//   onDelete = () => {},
+//   onComplete = () => {},
+// }: {
+//   task?: TTask;
+//   className?: string;
+//   badgeColor?: string;
+//   enableCompleteButton?: boolean;
+//   onDelete?: () => void;
+//   onComplete?: (taskId: string) => void;
+// }) {
+//   const { handleDelete, handleOpenDetailDialog, isDeleting } = useTaskDetail({
+//     task,
+//   });
 
-export default function TaskCard({
-  task,
-  className,
-  badgeColor,
-  enableCompleteButton,
-  onDelete = () => {},
-  onComplete = () => {},
-}: {
-  task?: TTask;
-  className?: string;
-  badgeColor?: string;
-  enableCompleteButton?: boolean;
-  onDelete?: () => void;
-  onComplete?: (taskId: string) => void;
-}) {
-  const { handleDelete, handleOpenDetailDialog, isDeleting } = useTaskDetail({
-    task,
-  });
+//   return (
+//     <article
+//       className={cn(
+//         "bg-white p-8 rounded-md hover:scale-[97.5%] transition-all duration-200 cursor-pointer",
+//         className,
+//         isDeleting && "opacity-50"
+//       )}
+//       onClick={(e) => handleOpenDetailDialog(e)}
+//     >
+//       <div className="flex gap-6 justify-between items-center">
+//         <div>
+//           <h2 className="text-md">{task?.title}.</h2>
+//           <div className="flex items-center justify-start whitespace-nowrap mt-2 gap-3">
+//             <p
+//               className="bg-dark px-4 py-[2px] rounded-full text-lightest"
+//               style={badgeColor ? { backgroundColor: badgeColor } : undefined}
+//             >
+//               + {task?.points} pts
+//             </p>
+//             <p className="text-light text">On Completion</p>
+//           </div>
+//         </div>
+//         <i
+//           className="text-md bx bx-trash text-lighter hover:text-dark cursor-pointer transition-all duration-200 self-start mt-2"
+//           onClick={() => {
+//             onDelete();
+//             handleDelete();
+//           }}
+//         ></i>
+//       </div>
+//       <p className="mt-8 text-light">{task?.description}</p>
+//       {enableCompleteButton && (
+//         <Button
+//           className="py-2 flex items-center justify-center gap-2 mt-5"
+//           onClick={() => {
+//             if (!task?.id) return;
+//             onComplete(task.id);
+//           }}
+//         >
+//           Complete <i className="text-md bx bx-check"></i>
+//         </Button>
+//       )}
+//     </article>
+//   );
+// }
+
+type TaskCardProps = {
+  task: TUserTask;
+};
+
+export default function OnGoingTaskCard({ task }: TaskCardProps) {
+  const { handleOpenDetailDialog, isDeleting } = useTaskDetail({ task });
 
   return (
     <article
       className={cn(
-        "bg-white p-8 rounded-md hover:scale-[97.5%] transition-all duration-200 cursor-pointer",
-        className,
+        "grid grid-cols-[auto_1fr] bg-white rounded-xl p-8 grid-rows-[12rem_4rem] gap-x-4 hover:scale-[97.5%] trasition-all duration-200 cursor-pointer sm:flex sm:flex-col",
         isDeleting && "opacity-50"
       )}
-      onClick={(e) => handleOpenDetailDialog(e)}
+      onClick={handleOpenDetailDialog}
     >
-      <div className="flex gap-6 justify-between items-center">
-        <div>
-          <h2 className="text-md">{task?.title}.</h2>
-          <div className="flex items-center justify-start whitespace-nowrap mt-2 gap-3">
-            <p
-              className="bg-dark px-4 py-[2px] rounded-full text-lightest"
-              style={badgeColor ? { backgroundColor: badgeColor } : undefined}
-            >
-              + {task?.points} pts
-            </p>
-            <p className="text-light text">On Completion</p>
-          </div>
-        </div>
-        <i
-          className="text-md bx bx-trash text-lighter hover:text-dark cursor-pointer transition-all duration-200 self-start mt-2"
-          onClick={() => {
-            onDelete();
-            handleDelete();
-          }}
-        ></i>
+      <div
+        className="row-span-3 w-2 h-2 rounded-full mt-3 sm:mb-6"
+        style={{ background: getMoodColor(task.mood as TMood) }}
+      ></div>
+      <div className="overflow-hidden">
+        <h3 className="text-md">{task.title}.</h3>
+        <p className="text-lighter mt-6 leading-[175%] line-clamp-3 sm:mb-10">
+          {task.description}
+        </p>
       </div>
-      <p className="mt-8 text-light">{task?.description}</p>
-      {enableCompleteButton && (
-        <Button
-          className="py-2 flex items-center justify-center gap-2 mt-5"
-          onClick={() => {
-            if (!task?.id) return;
-            onComplete(task.id);
-          }}
-        >
-          Complete <i className="text-md bx bx-check"></i>
-        </Button>
-      )}
+      <div className="text-lighter">
+        <p className="mb-2">On Completion</p>
+        <div className="bg-accent px-4 py-1 text-white w-fit rounded-full">
+          +{task.points} Points
+        </div>
+      </div>
     </article>
   );
 }

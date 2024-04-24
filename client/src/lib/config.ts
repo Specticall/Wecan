ff7f8defd4687e082410eb255776a76e15b72c95
Specticall <josephyusmita@gmail.com>
2024-04-24 22:08:18 +0700

    chore: moved things to .env

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,8 +1,8 @@
 export const BG_COLOR = "#f9f9ff";
 export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_ID;
 export const GOOGLE_CLIENT_SECRET = import.meta.env.VITE_GOOGLE_SECRET;
-export const BASE_ENDPOINT = "/api";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_ENDPOINT = import.meta.env.VITE_BASE_ENDPOINT;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const ACCENT_GRADIENT =
   "linear-gradient(163deg, rgba(74,90,239,1) 0%, rgba(122,75,255,1) 80%, rgba(250,174,119,1) 100%)";
